ae9dc76c9b6b73997e7b6265d5d731e9369f0175
DICP1810 <46855251+DICP1810@users.noreply.github.com>
2022-07-22 14:31:46 +0800

    Delete Task directory

--- Task/MSTaskQC.py
+++
@@ -1,9 +0,0 @@
-from System.MSData import CDataPack
-from Function.MSFunctionQC import CFunctionQCRank
-
-
-class CTaskRank:
-
-    def work(self, data_package=CDataPack()):
-        rank_function=CFunctionQCRank()
-        rank_function.rank(data_package)
